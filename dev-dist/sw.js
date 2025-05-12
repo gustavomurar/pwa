0aafc0f8c78b07f380c2311204946fd4b2a96b28
gustavomurar <gustavomurarosoares3@gmail.com>
2025-05-12 14:39:34 -0300

    iconeNovo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q65bk55lb1g"
+    "revision": "0.dm653n4gsgo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
