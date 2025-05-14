df8a3477b9f81ed240796a66130362643e8ac88f
gustavomurar <gustavomurarosoares3@gmail.com>
2025-05-14 15:47:38 -0300

    atualização

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dm653n4gsgo"
+    "revision": "0.h6sftsra6k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
